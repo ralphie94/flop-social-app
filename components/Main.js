b5e3322792a8c75169e39922f49c2731392ef115
Ralphie <ralphie94@pacbell.net>
2021-02-16 17:31:31 -0800

    added icons for bottom nav

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser } from "../redux/actions/index";
 
 import FeedScreen from "./main/Feed";
+import AddScreen from "./main/Add";
+import ProfileScreen from "./main/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,7 +22,24 @@ export class Main extends Component {
     render() {
         return (
             <Tab.Navigator>
-                <Tab.Screen name="Feed" component={FeedScreen} />
+                <Tab.Screen name="Feed" component={FeedScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="rabbit" color={color} size={26} />
+                    )
+                }} />
+                <Tab.Screen name="Add" component={AddScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="carrot" color={color} size={26} />
+                    )
+                }} />
+                <Tab.Screen name="Profile" component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="paw" color={color} size={26} />
+                    )
+                }} />
             </Tab.Navigator>
         )
     }
